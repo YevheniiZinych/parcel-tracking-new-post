6b563b4c5d21212f136fc97d39eb96362bff4714
ZEugenii <zinich911@gmail.com>
2023-03-10 09:04:46 +0200

    add getPostOffice operation

--- src/redux/PostOfficeSlice/operations.jsx
+++ src/redux/PostOfficeSlice/operations.jsx
@@ -5,29 +5,28 @@ const API_KEY = '28c234fe908d6729d7cc9731a29257d1';
 
 const baseURL = 'https://api.novaposhta.ua/v2.0/json/';
 
-export const getCurrentTracking = createAsyncThunk(
-  'parcel/fetchAll',
+export const getPostOffices = createAsyncThunk(
+  'office/fetchAll',
   async (data, thunkAPI) => {
-    const ttn = data.number;
+    // const ttn = data.number;
 
     const options = {
       apiKey: API_KEY,
-      modelName: 'TrackingDocument',
-      calledMethod: 'getStatusDocuments',
+      modelName: 'Address',
+      calledMethod: 'getWarehouses',
       methodProperties: {
-        Documents: [
-          {
-            DocumentNumber: ttn,
-          },
-        ],
+        CityName: 'Київ',
+        Page: '2',
+        Limit: '50',
+        Language: 'UA',
       },
     };
     try {
       const { data } = await axios.post(baseURL, options);
 
-      const { Status, WarehouseRecipient, WarehouseSender } = data.data[0];
+      //   const { Status, WarehouseRecipient, WarehouseSender } = data.data[0];
 
-      return { Status, WarehouseRecipient, WarehouseSender, ttn };
+      //   return { Status, WarehouseRecipient, WarehouseSender, ttn };
     } catch (e) {
       return thunkAPI.rejectWithValue(e.message);
     }
